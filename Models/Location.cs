41a7db8700343bb5786f8dfc5f6ab815c3c51faf
Vlatko <krstik1212@gmail.com>
2021-09-26 20:53:56 +0200

    full

---
+++ Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ApiBackend.Models
+{
+    public class Location
+    {
+        public long ID { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
+    }
+}
---
+++ Models/LocationDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBackend.Models
+{
+    public class LocationDB : DbContext
+    {
+        public LocationDB(DbContextOptions<LocationDB> options) : base(options)
+        { 
+        }
+
+        public DbSet<Location> Locations { get; set; }
+    }
+}
